a7fd0b37179932045e96202bc0c55cf6571dd5d4
Yurii Yaroshenko <yura12323@gmail.com>
2020-12-30 11:30:41 +0200

    Add frontend version.

--- back/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a20e58562d7788b3c1eaa0429afa1c83'),
-    },
-  },
-});
--- back/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'bf041025-4436-43f4-b26b-63a9a7a62993'
-};
