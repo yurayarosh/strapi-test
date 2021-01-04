fc9619172e52d4dcdc95e5a4075195afa0ea98bf
Yurii Yaroshenko <yura12323@gmail.com>
2021-01-04 11:42:10 +0200

    add trycatch to fetching routes

--- router.js
+++ router.js
@@ -12,51 +12,55 @@ Vue.use(Router)
 
 const getRoutes = async (collection = 'pages', component) => {
   const { default: def, languages } = langConfig
-  const response = await axios.get(`${process.env.BASE_URL_BACK}/${collection}`)
-  const { data: items } = response
-  let pageType
-
-  switch (collection) {
-    case PAGES:
-      pageType = PAGE
-      break
-    case POSTS:
-      pageType = POST
-      break
-    case PRODUCTS:
-      pageType = PRODUCT
-      break
-    default:
-      pageType = PAGE
-      break
-  }
-
   const routes = []
 
-  languages.forEach(language => {
-    items.forEach(({ alias, id }) => {
-      if (!alias) alias = ''
-      const langName = language === 'uk' ? 'ua' : language
-      const subdir = language === def || !langName ? '' : `/${langName}`
-      let name = ''
+  try {
+    const response = await axios.get(`${process.env.BASE_URL_BACK}/${collection}`)
+    const { data: items } = response
+    let pageType
 
-      if (alias) name = language === def ? alias : `${alias}---${langName}`
-      if (alias === '') name = language === def ? 'home' : `home---${langName}`
+    switch (collection) {
+      case PAGES:
+        pageType = PAGE
+        break
+      case POSTS:
+        pageType = POST
+        break
+      case PRODUCTS:
+        pageType = PRODUCT
+        break
+      default:
+        pageType = PAGE
+        break
+    }
 
-      routes.push({
-        name,
-        component,
-        path: `${subdir}/${alias}`,
-        meta: {
-          id,
-          language,
-          type: pageType,
-        },
+    languages.forEach(language => {
+      items.forEach(({ alias, id }) => {
+        if (!alias) alias = ''
+        const langName = language === 'uk' ? 'ua' : language
+        const subdir = language === def || !langName ? '' : `/${langName}`
+        let name = ''
+
+        if (alias) name = language === def ? alias : `${alias}---${langName}`
+        if (alias === '') name = language === def ? 'home' : `home---${langName}`
+
+        routes.push({
+          name,
+          component,
+          path: `${subdir}/${alias}`,
+          meta: {
+            id,
+            language,
+            type: pageType,
+          },
+        })
       })
     })
-  })
 
-  console.log({ routes })
+    console.log({ routes })
+  } catch (error) {
+    console.error('Fetching routes error', error)
+  }
 
   return routes
 }
