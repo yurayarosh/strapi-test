80f366e79bdc3262a0e036c44a4ee8cfeca0c2b6
Yurii Yaroshenko <yura12323@gmail.com>
2021-06-23 11:23:25 +0300

    [BACKEND]: Change cloudinary provider.

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'cloudinary',
+    provider: 'cloudinary-folderoptions',
     providerOptions: {
       cloud_name: env('CLOUDINARY_NAME'),
       api_key: env('CLOUDINARY_KEY'),
       api_secret: env('CLOUDINARY_SECRET'),
+      default_folder: env('CLOUDINARY_DEFAULT_FOLDER'),
     },
   },
 });
